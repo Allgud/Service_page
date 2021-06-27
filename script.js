92014661b7a3e191f0f971175c2dde53cbb21097
Alexander <allgud83@gmail.com>
2021-06-27 14:14:01 +0300

    Все разрешения, кроме слайдера

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-new Swiper('.list', {
+/*new Swiper ('.software__list', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 
-    slidesPerView: 'auto',
-});
+    slidesPerView: 'auto'
+})*/
+
+
+let hiddenBlock = document.querySelector('.list__block--hidden')
+let arrowButton = document.querySelector('.dropdown__button img')
+let showHiddenHandler = document.querySelector('.dropdown__span')
+
+showHiddenHandler.addEventListener('click', () => {
+    if(showHiddenHandler.textContent === "Показать все"){
+        hiddenBlock.style.display = 'flex'
+        showHiddenHandler.textContent = 'Скрыть'
+        arrowButton.style.transform = "rotate(180deg)"
+    }else {
+        hiddenBlock.style.display = 'none'
+        showHiddenHandler.textContent = 'Показать все'
+        arrowButton.style.transform = "rotate(0deg)" 
+    }
+})
 
